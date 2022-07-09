0d1c2fc252dcfca3eff150e495d75a6ed0586043
jobkung555 <jobkung555@gmail.com>
2022-07-10 00:22:57 +0700

    update

--- js/js.js
+++ js/js.js
@@ -1,13 +1,26 @@
 $(function () {
     function scrollside() {
-        var scroll = $(window).scrollTop() + $(window).outerHeight() - 100
-        console.log(scroll)
-        $(".scollnep").each(function (index) {
-            $(this).toggleClass("is-nep", !!(scroll >= $(this).offset().top))
+        var scroll = $(window).scrollTop() 
+        let scrollarea = scroll + $(window).outerHeight() - 100
+        $(".scollnep").each(function () {
+            $(this).toggleClass("is-nep", !!(scrollarea >= $(this).offset().top))
+        });
+        $(".nav-link").each(function (index) {
+            let sectop = $($(this).attr("href")).offset().top - 100
+            let secbot = $($(this).attr("href")).offset().top + $($(this).attr("href")).height()
+            $(this).toggleClass("active", !!(sectop < scroll && scroll < secbot))
         });
     }
     $(window).scroll(function () {
         scrollside()
     })
     scrollside()
+
+    $(".nav-link").click(function (e) {
+        e.preventDefault()
+        $("html,body").animate({ scrollTop: $($(this).attr("href")).offset().top });
+    })
+    $(".header .icon-menu").click(function (e) {
+        $(".header .menu").toggleClass("active")
+    })
 })
